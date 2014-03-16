de5c52e26ee9f6d7d656587cd7942a6ecfce0c9e
Wendy Wang <wilxnwang@gmail.com>
2014-03-16 13:57:41 +0800

    Fix some bugs

--- source/javascripts/map.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-  var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
-};
